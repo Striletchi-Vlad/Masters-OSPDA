4c28df16451b9bc296fbf1a3773a60f6144a6167
Tzepesh <vlad.striletchi@airportlabs.com>
2025-01-11 16:55:52 +0200

    benchmark w/ multiple compute nodes

--- shared/hello_mpi.c
+++ shared/hello_mpi.c
@@ -1,5 +1,8 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <unistd.h> // For sleep()
+
+#define TOTAL_TASKS 10 // Predefined total number of sleep tasks
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -8,7 +11,20 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    printf("Hello from processor %d of %d\n", world_rank, world_size);
+    // Calculate number of tasks per process and the remainder
+    int tasks_per_process = TOTAL_TASKS / world_size;
+    int extra_tasks = TOTAL_TASKS % world_size;
+
+    // Determine the number of tasks for this process
+    int num_tasks = tasks_per_process + (world_rank < extra_tasks ? 1 : 0);
+
+    printf("Process %d performing %d tasks\n", world_rank, num_tasks);
+
+    // Perform the assigned sleep tasks
+    for (int i = 0; i < num_tasks; i++) {
+        printf("Process %d performing task %d\n", world_rank, i + 1);
+        sleep(1); // Sleep for 1 second
+    }
 
     MPI_Finalize();
     return 0;
